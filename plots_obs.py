30795572a276e3c96cc8d01ef6eb8678769d2851
marcosv9 <marcosv9@hotmail.com>
2021-12-02 10:47:12 -0300

    Update plots_obs.py

--- plots_obs.py
+++ plots_obs.py
@@ -3,10 +3,186 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from numba import jit, njit
 import numba
+import glob
+from glob import glob
+import os
+import ftplib
+import pathlib
+import matplotlib.gridspec as gridspec
+from datetime import datetime
+import pwlf
+from matplotlib.widgets import Slider, Button
 
 #funções para plotar dados de observatórios, ainda em desenvovilmento
 #adicionar funções para conversão H,D e Z para X, Y e Z, conversão dados sec para min.
 
+
+def resample_obs_data(dataframe, sample):
+    df = pd.DataFrame()
+    df = dataframe
+    samples = ['H','D','M','Y']
+    
+    if sample not in samples:
+        print('sample must be H, D, M or Y!')
+    else:
+        if sample == 'H':
+            df = (df.resample('H').mean()).shift(30, freq = 'Min')
+        if sample == 'D':
+            df = df.resample('D').mean().shift(12, freq = 'H')
+        if sample == 'M':
+            df = df.resample('M').mean().shift(-15, freq = 'D')
+        if sample == 'Y':
+            df = df.resample('Y').mean().shift(-182.5, freq = 'D')
+            
+        return df
+
+
+def load_qd_data(station, path):
+    '''
+    Function to read and concat observatory data
+    
+    Sample must be H, D, M, Y   
+    
+    '''
+    #print('Reading files from '+ station.upper() +'...')
+    #year  = []
+    #for i in range(int(starttime[0:4]),int(endtime[0:4])+ 1):
+    #    Y = i
+    #    year.append(Y)
+    #
+    #Years = []
+    #Years.extend([str(i) for i in year])
+    #Years
+    #
+    #files_station = []
+    #
+    L_27 = ['CZT','DRV','PAF']
+    L_26 = ['NGK','DRV','MAW','CNB','HAD','TSU','HON','KAK','BOU','KOU','HBK','BMT']
+    #
+    skiprows = 17
+    if station.upper() in L_27:
+        skiprows = 27
+    if station.upper() in L_26:
+        skiprows = 26
+    #
+    #for Year in Years:
+
+    files_station = glob(path + '/*')
+    files_station.sort()
+
+    #d_parser = lambda x: datetime.strptime(x, '%Y-%m-%d %H:%M:%S%.f')
+    
+    df_station = pd.concat( (pd.read_csv(file, sep='\s+',usecols = [0,1,3,4,5,6], 
+                   header = None,skiprows = skiprows, 
+                   parse_dates = {'Date': ['date', 'Time']},
+                   names = ['date','Time','X','Y','Z','F']) for file in files_station), 
+                   ignore_index = True)
+    df_station['Date'] = pd.to_datetime(df_station['Date'], format = '%Y-%m-%dd %H:%M:%S.%f')     
+    #df_station['Hour'] = pd.to_datetime(df_station['Hour'], format = '%H:%M:%S.%f').dt.time               
+    df_station.set_index('Date', inplace = True)
+
+    
+    df_station.loc[df_station['X'] == 99999.0, 'X'] = np.nan
+    df_station.loc[df_station['Y'] == 99999.0, 'Y'] = np.nan
+    df_station.loc[df_station['Z'] == 99999.0, 'Z'] = np.nan 
+    df_station.loc[df_station['F'] == 88888.0, 'F'] = np.nan 
+    
+    return df_station
+
+def obs_data_processing(station,path, starttime, endtime, sample = None, plots: bool = False):
+    
+    df = load_qd_data(station = station, path = path)
+    df = df.loc[starttime:endtime]
+    if sample == None:
+        df = df
+    else:
+        df = resample_obs_data(df, sample = sample)
+    if plots == False:
+        pass
+    if plots == True:
+        #components = ['X','Y','Z','F']
+        colors = ['blue','green','black','red']
+        fig, axes = plt.subplots(4,1, figsize = (14,10))
+        for col, ax, color in zip(df.columns, axes.flatten(), colors):
+            ax.plot(df[col], color  = color)
+            ax.set_ylabel(col.upper() + '/nT', fontsize = 12)
+            
+                 
+     
+    
+    return df
+
+
+
+
+def p_diff_obs(obs, pillar,  starttime = None, endtime = None):
+    
+    if obs == 'ttb':
+        path = 'planilhas/Pillar diff ttb0 since 2019.xlsx'
+    if obs == 'vss':
+        path = 'planilhas/Pillar diff ttb0 since 2019.xlsx'
+        
+    df_obs =  pd.read_excel(path, usecols = [0,2,3,4,5,6], names = ['Date','Pillar 1','Pillar 2','Pillar Diff', 'JD','DOY'])
+    df_obs['Date'] = pd.to_datetime(df_obs['Date'], format = '%Y%m%d')
+    df_obs.set_index('Date', inplace = True)
+    
+    if starttime == None and endtime == None:
+    
+        fig, ax = plt.subplots(figsize=(18,5)) 
+    #gs = gridspec.GridSpec(2, 1, height_ratios=[3, 1]) 
+    #Days = ['01/01','03/01','05/01','07/01','09/01','11/01']
+    #ax0 = plt.subplot(gs[0])
+        ax.plot(df_obs['Pillar ' + str(pillar)], 'or',markersize = 5, label = 'Pillar ' + str(pillar))
+#ax.plot(df1['jd'],pl(df1['jd']),'k--',label = 'linear trend = 0.002429x - 50.53', markersize = 4)
+#ax[0].plot(df2['doy'], df2['over'] , 'o--',label = 'GSM Over',markersize = 5)
+#ax[0].plot(DOY,pl(DOY),'r-',label = 'trend')
+#ax[0].set_xticklabels(Days)
+#ax.set_ylim(-35,-28)
+#ax[0].set_xticks(np.arange(0,365,61))
+#ax[0].axhline(y=np.mean(Diff), c='k', linewidth=2, alpha=0.5, linestyle='--',label = 'mean =-32,21 nT')
+#ax.set_xlim(7252,7669)
+        ax.set_ylabel('Differences(nT)', fontsize = 14)
+        ax.set_xlabel('Time', fontsize = 14)
+        ax.set_ylim(1.05*df_obs['Pillar ' + str(pillar)].min(),0.95*df_obs['Pillar ' + str(pillar)].max())
+        ax.set_title('Pillar differences ' + obs.upper(), fontsize = 16)
+        ax.grid()
+        ax.legend(loc='best', fontsize = 12)
+        plt.show()
+        
+        print('The maximum difference is:', round(df_obs['Pillar ' + str(pillar)].max(),3))
+        print('The minimum difference is:', round(df_obs['Pillar ' + str(pillar)].min(),3))
+        print('The median:', round(df_obs['Pillar ' + str(pillar)].median(),3))
+        print('The mean is:', round(df_obs['Pillar ' + str(pillar)].mean(),3))
+        print('The STD is:', round(df_obs['Pillar ' + str(pillar)].std(),3))
+        
+    else:
+        fig, ax = plt.subplots(figsize=(18,5)) 
+    #gs = gridspec.GridSpec(2, 1, height_ratios=[3, 1]) 
+    #Days = ['01/01','03/01','05/01','07/01','09/01','11/01']
+    #ax0 = plt.subplot(gs[0])
+        ax.plot(df_obs['Pillar ' + str(pillar)][starttime:endtime], 'or',markersize = 5, label = 'Pillar ' + str(pillar))
+#ax.plot(df1['jd'],pl(df1['jd']),'k--',label = 'linear trend = 0.002429x - 50.53', markersize = 4)
+#ax[0].plot(df2['doy'], df2['over'] , 'o--',label = 'GSM Over',markersize = 5)
+#ax[0].plot(DOY,pl(DOY),'r-',label = 'trend')
+#ax[0].set_xticklabels(Days)
+#ax.set_ylim(-35,-28)
+#ax[0].set_xticks(np.arange(0,365,61))
+#ax[0].axhline(y=np.mean(Diff), c='k', linewidth=2, alpha=0.5, linestyle='--',label = 'mean =-32,21 nT')
+#ax.set_xlim(7252,7669)
+        ax.set_ylabel('Differences(nT)', fontsize = 14)
+        ax.set_ylim(1.05*df_obs['Pillar ' + str(pillar)][starttime:endtime].min(),0.95*df_obs['Pillar ' + str(pillar)][starttime:endtime].max())
+        ax.set_xlabel('Time', fontsize = 14)
+        ax.set_title('Pillar differences ' + obs.upper(), fontsize = 16)
+        ax.grid()
+        ax.legend(loc='best', fontsize = 12)
+        plt.show()
+        
+        print('The maximum difference is:', round(df_obs['Pillar ' + str(pillar)][starttime:endtime].max(),3))
+        print('The minimum difference is:', round(df_obs['Pillar ' + str(pillar)][starttime:endtime].min(),3))
+        print('The median:', round(df_obs['Pillar ' + str(pillar)][starttime:endtime].median(),3))
+        print('The mean is:', round(df_obs['Pillar ' + str(pillar)][starttime:endtime].mean(),3))
+        print('The STD is:', round(df_obs['Pillar ' + str(pillar)][starttime:endtime].std(),3))
+        
 
 def plot_components_sep(x,y,z,label1,label2,label3,title, ndays, days):
     fig, ax = plt.subplots(3,1,figsize=(16, 10)) 
@@ -259,7 +435,6 @@ def hampel_filter_forloop_numba(input_series, window_size, n_sigmas=3):
     fig, ax = plt.subplots(figsize = (16,5))
     ax.plot(input_series, 'k', label = 'Removed Outliers')
     ax.plot(new_series, 'r', label = 'New Series')
-    ax.set_xlim(0,new_series.size)
     ax.legend(loc='center left', bbox_to_anchor=(1.0, 0.5), fontsize = 12)
     plt.grid()
     plt.show()
